23daec41cee01430f68603ded9bac787223f4a3f
Patricks-Hub <83692732+dm1pfoy188@users.noreply.github.com>
2025-03-20 13:34:14 -0500

    removed test program

--- Personal Python Projects/Weather/testimage.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-try:
-    img = Image.open("C:\College Work\CIS189 - Python I\Personal projects\Weather\images\daytime.jpg")
-    print("Image opened successfully!")
-except FileNotFoundError:
-    print("Image not found!")
